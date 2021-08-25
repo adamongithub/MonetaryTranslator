cc6c47b51ca53951f0bb5184fa29e98dc3cb07af
Adam Taylor <adam.taylor@navico.com>
2021-08-25 16:13:55 +0100

    Add input validation method

--- MonetaryTranslator/Program.cs
+++ MonetaryTranslator/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.Globalization;
 
 namespace MonetaryTranslator
 {
     class Program
     {
+        private static CultureInfo _Culture = CultureInfo.CreateSpecificCulture("en-GB");
+
         static void Main(string[] args)
         {
             
         }
 
+        private static bool InputIsValid(string input)
+        {
+            if (decimal.TryParse(input, NumberStyles.Currency, _Culture, out decimal d)
+                && d >= 0
+                && d <= 999999999)
+            {
+                return true;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Number cannot be negative and must be smaller or equal to 10^9.");
+            }
+        }
     }
 }
